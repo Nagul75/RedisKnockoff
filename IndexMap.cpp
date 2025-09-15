e466a0133643e27607108808091a3200a8f533fa
Nagul75 <nagul697@gmail.com>
2025-09-15 15:16:49 +0530

    Implement constructor, add and remove indices

--- IndexMap.cpp
+++ IndexMap.cpp
@@ -1,3 +1,66 @@
 //
 // Created by regal on 9/15/25.
 //
+
+#include "IndexMap.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+
+RK::IndexMap::IndexMap(const std::string& instance)
+    :m_instance(instance)
+{
+    m_indices.clear();
+    m_indexFile.open(instance, std::ios::in | std::ios::out | std::ios::app);
+    if (!m_indexFile.is_open())
+    {
+        std::cerr << "Error opening index \n";
+    }
+    std::string line{};
+    while (std::getline(m_indexFile, line))
+    {
+        std::stringstream ss{line};
+        std::string cell{};
+        std::vector<std::string> row{};
+        while (std::getline(ss, cell, ','))
+        {
+            row.emplace_back(cell);
+        }
+
+        m_indices[row[0]] = std::stoll(row[1]);
+    }
+    m_indexFile.close();
+    std::cout << "Finished reading file \n";
+}
+
+
+RK::IndexMap::~IndexMap()
+{
+    std::cout << "Saving index file.\n";
+    m_indexFile.open(m_instance, std::ios::out | std::ios::trunc); // Overwrite the file on shutdown
+    if (m_indexFile.is_open())
+    {
+        for (const auto& pair : m_indices)
+        {
+            m_indexFile << pair.first << "," << pair.second << '\n';
+        }
+        m_indexFile.close();
+        std::cout << "Index file saved successfully.\n";
+    }
+    else
+    {
+        std::cerr << "Error saving index file.\n";
+    }
+}
+
+void RK::IndexMap::addIndex(const std::string& key, const std::streampos pos)
+{
+    m_indices[key] = pos;
+}
+
+void RK::IndexMap::removeIndex(const std::string& key)
+{
+    m_indices.erase(key);
+}
--- IndexMap.h
+++ IndexMap.h
@@ -5,4 +5,26 @@
 #ifndef REDISKNOCKOFF_INDEXMAP_H
 #define REDISKNOCKOFF_INDEXMAP_H
 
+#include <unordered_map>
+#include <string>
+#include <fstream>
+namespace RK
+{
+    class IndexMap
+    {
+    public:
+        explicit IndexMap(const std::string& instance);
+        ~IndexMap();
+
+        std::unordered_map<std::string, std::streampos>& getIndices(){return m_indices;}
+
+        void addIndex(const std::string& key, std::streampos pos);
+        void removeIndex(const std::string& key);
+    private:
+        std::unordered_map<std::string, std::streampos> m_indices{}; // key : bytes
+        std::fstream m_indexFile{};
+        std::string m_instance{};
+    };
+}
+
 #endif //REDISKNOCKOFF_INDEXMAP_H
